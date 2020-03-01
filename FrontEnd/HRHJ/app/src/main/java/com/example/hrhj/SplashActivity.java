3d9b9c270f7e23d4d683f5a009e64c10a1c83aaf
yoonh-j <akopysj177@naver.com>
2020-03-02 07:11:40 +0900

    UI Updated
    - 스플래시 변경
    - 앱 아이콘 변경

--- FrontEnd/HRHJ/app/src/main/java/com/example/hrhj/SplashActivity.java
+++ FrontEnd/HRHJ/app/src/main/java/com/example/hrhj/SplashActivity.java
@@ -11,7 +11,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
         SharedPreferences sharedPreferences = getSharedPreferences("UserData",MODE_PRIVATE);
 
